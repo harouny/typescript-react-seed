9ac77e9bc9769fb8ed415f5531b1934183cc1257
Ahmed Elharouny <Ahmed.Elharouny@Nintex.com>
2017-10-05 16:10:46 +1100

    initial project structure

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CommonsChunkPlugin } = require('webpack').optimize;
+const outputPath = path.join(process.cwd(), "dist");
+const sourcePath = path.join(process.cwd(), "src");
+
+module.exports = {
+    entry: {
+        bundle: ['./src/index.tsx']
+      }, 
+    
+    output: {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: outputPath
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            { test: /\.ts(x?)$/, exclude: /node_modules/, use: ['babel-loader', 'ts-loader'] },
+            { enforce: "pre", test: /\.js$/, use: ['source-map-loader'] },
+            { test: /\.js$/, use:['babel-loader'], exclude: /node_modules/ },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            "template": path.join(sourcePath, "index.html"),
+            "filename": path.join(outputPath, "index.html"),
+          }),
+          new CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: (module) => module.context.indexOf && module.context.indexOf('node_modules') !== -1,
+          })
+        ],
+    devServer: {
+        "contentBase": "./dist",
+    }
+};
